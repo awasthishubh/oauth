9d74a1fb50bcb62335c336ccf858baa0913ec908
Shubham Awasthi <shubham0522@gmail.com>
2018-05-31 18:56:31 +0530

    Added bodyparser

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.use((req,res)=>{
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
 })
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 require('./routes/google').google(app);
 
 app.get('/', (req, res) => {

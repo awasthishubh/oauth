c2286296acce54e48758269b078144497c4de71c
Shubham Awasthi <shubham0522@gmail.com>
2018-05-31 22:59:45 +0530

    Refracting code

--- routes/google.js
+++ routes/google.js
@@ -25,28 +25,11 @@ function google(app) {
         code,
         redirect_uri: 'http://localhost:3000/google/callback'
       }
-    }, function(err, httpResponse, body) {
+    }, async function(err, httpResponse, body) {
       // res.send(body)
       try {
-        request.get({
-          url: 'https://www.googleapis.com/oauth2/v2/userinfo',
-          json: true,
-          'auth': {
-            'bearer': body.access_token
-          }
-        }, function(err, httpResponse, data) {
-          // res.send(data)
-          request.get({
-            url: 'https://www.googleapis.com/plus/v1/people/' + data.id,
-            json: true,
-            'auth': {
-              'bearer': body.access_token
-            }
-          }, function(err, httpResponse, data2) {
-            res.send(data2)
-            return data2.id
-          });
-        });
+        usid= await require('./services/googledata').data(body.access_token);
+        return res.json({usid})
       } catch (err) {
         return res.json({
           err: 'Invalid/Missing auth code'
@@ -54,7 +37,7 @@ function google(app) {
       }
     })
     //
-    console.log("dfdfdfdf");
+    // console.log("dfdfdfdf");
   })
 }
 module.exports.google = google
--- routes/services/googledata.js
+++ routes/services/googledata.js
@@ -1,21 +1,25 @@
-function basic(access_token) {
+const request = require('request');
+function basicdata(access_token) {
+  console.log(access_token);
   request.get({
     url: 'https://www.googleapis.com/oauth2/v2/userinfo',
     json: true,
     'auth': {
-      'bearer': body.access_token
+      'bearer': access_token
     }
   }, function(err, httpResponse, data) {
-    // res.send(data)
+    console.log(data)
     request.get({
       url: 'https://www.googleapis.com/plus/v1/people/' + data.id,
       json: true,
       'auth': {
-        'bearer': body.access_token
+        'bearer': access_token
       }
     }, function(err, httpResponse, data2) {
-      res.send(data2)
+      console.log(data2)
       return data2.id
     });
   });
 }
+
+module.exports.data = basicdata;

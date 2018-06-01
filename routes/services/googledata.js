8356058f34742926c7b41e3b85773c3711d29d76
Shubham Awasthi <shubham0522@gmail.com>
2018-06-01 17:21:57 +0530

    Improved google oauth code

--- routes/services/googledata.js
+++ routes/services/googledata.js
@@ -4,6 +4,7 @@ function basicdata(access_token) {
   return new Promise(function(resolve, reject) {
     if(!access_token) reject()
     console.log(access_token);
+
     request.get({
       url: 'https://www.googleapis.com/oauth2/v2/userinfo',
       json: true,
@@ -13,16 +14,18 @@ function basicdata(access_token) {
     }, function(err, httpResponse, data) {
       if(!data) reject()
       console.log(data)
-      request.get({
-        url: 'https://www.googleapis.com/plus/v1/people/' + data.id,
-        json: true,
-        'auth': {
-          'bearer': access_token
-        }
-      }, function(err, httpResponse, data2) {
-        console.log(data2)
-        resolve(data2.id)
-      });
+    });
+
+    request.get({
+      url: 'https://www.googleapis.com/plus/v1/people/me',
+      json: true,
+      'auth': {
+        'bearer': access_token
+      }
+    }, function(err, httpResponse, data) {
+      if(!data) reject()
+      console.log(data)
+      resolve(data.id)
     });
   });
 }

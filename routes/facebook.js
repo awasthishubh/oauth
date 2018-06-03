1bcb0f46f45d42ff90e6d280ce58dbe143bbb041
Shubham Awasthi <shubham0522@gmail.com>
2018-06-03 19:01:38 +0530

    Fb auth comp

--- routes/facebook.js
+++ routes/facebook.js
@@ -1,27 +1,33 @@
+const request = require('request');
+const authkeys = require('../config/keys').auth;
+
 function facebook(app) {
   app.all('/facebook', (req, res) => {
-    code = req.query.code;
-    req
-    res.status(400).send({
-      req: req.query,
-      rex: req.body
-    })
 
-    // res.send()
-    req.query.code
+    url=`https://www.facebook.com/v3.0/dialog/oauth?client_id=${authkeys.facebook.client_key}&redirect_uri=${authkeys.facebook.callback}&response_type=code&scope=email`
+
+    res.writeHead(303,
+      {Location: url}
+    );
+    res.end();
   })
 
-https://graph.facebook.com/v2.10/oauth/access_token?client_id=1955990028046787&redirect_uri=https://acm-reachout.herokuapp.com/&client_secret=fb88747368f6ffed594d892325e09e25&code=AQBMm2qq-6TDMOaXlwJmUW2lybgAHcb0NmG0cGv6ssNBuS4cFvq0jlS6KvTj050KHYakEQzbam7GDqX4XEWihzq8FhqfDzHAY0y08Ie4NXgRxauIuk3kvsJhhQBrmmL3hr6878-xkXVLTzSGC8QrkT2BUWFFsXgMtti3WOWcyFSNyrCEskEPGw27GtnUw2vavrxht1j7tyqLQtELViSGe_SIlwxPQLYE5HnCzvewOl8Yz8YSl8p7C7r1da39j5z_btgYc_A1qEmT0o6Q1TAGJOuUe7YoodBktHaX0_1s_M0bgkEOFs3Uq9aeX0fGlp0wDT15VIMeHNl0xvNK8foNgJcZ#_=_
   app.all('/facebook/callback', (req, res) => {
     code=req.query.code;
     const request = require('request');
     request.get({
-      url:`https://graph.facebook.com/v2.10/oauth/access_token?client_id=1955990028046787&redirect_uri=https://acm-reachout.herokuapp.com/&client_secret=fb88747368f6ffed594d892325e09e25&code=${code}`,
+      url:`https://graph.facebook.com/v2.10/oauth/access_token?client_id=${authkeys.facebook.client_key}&redirect_uri=${authkeys.facebook.callback}&client_secret=${authkeys.facebook.secrete_key}&code=${code}`,
       json:true
     }, async (err, httpResponse, body)=>{
       console.log(body);
-      data= await require('./services/facebookdata').data(body.access_token)
-      return res.json({data})
+      try{
+        data= await require('./services/facebookdata').data(body.access_token)
+        return res.json({data})
+      } catch (err) {
+        return res.json({
+          err: 'Invalid/Missing auth code'
+        })
+      }
     })
 
 
--- routes/services/facebookdata.js
+++ routes/services/facebookdata.js
@@ -7,7 +7,8 @@ function basicdata(access_token) {
       url: 'https://graph.facebook.com/me?access_token='+access_token+'&fields=id,name,email',
       json:true,
     }, function(err, httpResponse, data) {
-      console.log('data');
+      if(!data) reject()
+      else if(!(data.id)) reject()
       console.log(data);
       resolve(data.id)
     })
@@ -19,37 +20,6 @@ function basicdata(access_token) {
       console.log(data);
     })
   })
-
-
-
 }
-
-
-// function basicdata(access_token) {
-//   return new Promise(function(resolve, reject) {
-//     if(!access_token) reject()
-//     console.log(access_token);
-//     request.get({
-//       url: 'https://www.googleapis.com/oauth2/v2/userinfo',
-//       json: true,
-//       'auth': {
-//         'bearer': access_token
-//       }
-//     }, function(err, httpResponse, data) {
-//       if(!data.id) reject()
-//       console.log(data)
-//       request.get({
-//         url: 'https://www.googleapis.com/plus/v1/people/' + data.id,
-//         json: true,
-//         'auth': {
-//           'bearer': access_token
-//         }
-//       }, function(err, httpResponse, data2) {
-//         console.log(data2)
-//         resolve(data2.id)
-//       });
-//     });
-//   });
-// }
 
 module.exports.data = basicdata;

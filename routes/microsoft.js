7c0e8e50ca6d1d04a656ac63241a805955fd1841
Shubham Awasthi <shubham0522@gmail.com>
2018-06-03 18:48:09 +0530

    Ms oauth completed

--- routes/microsoft.js
+++ routes/microsoft.js
@@ -3,36 +3,46 @@ const authkeys = require('../config/keys').auth;
 
 function microsoft(app) {
   app.all('/microsoft', (req, res) => {
-    code = req.query.code;
-    req
-    res.status(400).send({
-      req: req.query,
-      rex: req.body
-    })
 
-    // res.send()
-    req.query.code
+    url = `https://login.microsoftonline.com/common/oauth2/v2.0/authorize\
+    ?client_id=${authkeys.microsoft.client_key}\
+    &response_type=code\
+    &scope=user.read\
+    &redirect_uri=${authkeys.microsoft.callback}\
+    &response_mode=query`
+
+    res.writeHead(303, {
+      Location: url
+    });
+    res.end();
   })
 
   app.all('/microsoft/callback', (req, res) => {
-    code=req.query.code;
+    code = req.query.code;
     request.get({
-      url:`https://login.microsoftonline.com/common/oauth2/v2.0/token`,
-      json:true,
+      url: `https://login.microsoftonline.com/common/oauth2/v2.0/token`,
+      json: true,
       form: {
         grant_type: 'authorization_code',
         code,
         client_id: authkeys.microsoft.client_key,
         client_secret: authkeys.microsoft.secrete_key,
-        redirect_uri: "http://localhost:3000/microsoft/callback",
+        redirect_uri: authkeys.microsoft.callback,
         scope: "user.read"
       }
-    }, async (err, httpResponse, body)=>{
-      console.log(body);
-      data= await require('./services/microsoftdata').data(body.access_token)
-      return res.json({data})
+    }, async (err, httpResponse, body) => {
+      try {
+        console.log(body);
+        data = await require('./services/microsoftdata').data(body.access_token)
+        return res.json({
+          data
+        })
+      } catch (err) {
+        return res.json({
+          err: 'Invalid/Missing auth code'
+        })
+      }
     })
-
   })
 }
 module.exports.microsoft = microsoft
